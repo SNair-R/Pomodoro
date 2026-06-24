32275c94f2eca76fa90863e8b4895b98b1277316
SNair-R <sidharthnair4306@gmail.com>
2026-06-23 22:25:16 -0500

    Add SSD1306 I2C driver and example code for OLED display
    - Implemented SSD1306 I2C driver in ssd1306_i2c.c and corresponding header in ssd1306_i2c.h
    - Added functions for initializing the display, sending commands, and rendering graphics
    - Included basic drawing functions such as SetPixel and DrawLine
    - Created example projects for Motion LED detection and PC temperature monitoring
    - Added CMakeLists.txt files for building projects
    - Implemented LCD 1602 I2C driver for displaying ADC readings
    - Added servo control example using PWM and ADC for angle adjustment
    - Created separate projects for potentiometer reading and servo control

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "lcd_1602_i2c.h"
+#include "pico/binary_info.h"
+#include "hardware/adc.h"
+
+#define PICO_I2C_SDA_PIN 14
+#define PICO_I2C_SCL_PIN 15
+#define READER_PIN 27
+
+
+int main() {
+    
+    stdio_init_all();
+    sleep_ms(2000);
+    adc_init();
+    adc_gpio_init(READER_PIN);
+    adc_select_input(1);
+    i2c_init(i2c1, 100 * 1000);
+    gpio_set_function(PICO_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_I2C_SDA_PIN);
+    gpio_pull_up(PICO_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_I2C_SDA_PIN, PICO_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    lcd_init();
+
+    while(true)
+    {
+        uint16_t pot_value = adc_read();
+    
+        char buf[17]; // 16 chars + null terminator
+        snprintf(buf, sizeof(buf), "Value: %d", pot_value);
+        lcd_clear();
+        lcd_set_cursor(0, 0);
+        lcd_string(buf);
+        sleep_ms(200);
+    }
+
+}
